5131fba99b7d3ae0c33c645e45237be06cee68fe
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-02-08 23:35:41 -0500

    Create ElasticSearchService and some docs
    Initial setup of the SearchService so I can get something started,
    created a not-yet-complete SearchRequest object and a simple trait that
    I'll use to Cake-ify the Service within the tests to use an embedded
    node and that I'll indicate one could use with a transport client (or
    node client) in the docs / regular code.

---
+++ src/main/scala/ElasticSearchService.scala
@@ -0,0 +1,41 @@
+package com.github.edgecaseberg.elasticexamples
+
+import org.elasticsearch.client.Client
+import org.elasticsearch.index.query._
+
+/** Extend this trait and provide a configured client, then mix it into ElasticSearchService */
+trait ElasticClient {
+	val client: Client
+}
+
+/** Helper Class to make our service interface a little bit easier */
+case class SearchRequest(
+	index: String,
+	docType: Option[String] = None,
+	keywords: Option[String] = None
+)
+
+/** Service with methods to perform search requests against an ElasticSearch instance
+ *
+ *  To instantiate you need to mix in an ElasticClient that provides a Client
+ *  to query.
+ *  {{{
+ *  trait ECImpl { val client = ... }
+ *  val elasticSearchService = new ElasticSearchService wih ECImpl
+ *  //use elasticSearchService which will query the client configured in ECImpl
+ *  }}}
+ *
+ */
+class ElasticSearchService {
+	this: ElasticClient =>
+
+	def matchAll(searchRequest: SearchRequest) = {
+		val queryBuilder = client.prepareSearch(searchRequest.index)
+		searchRequest.docType.map { docType =>
+			queryBuilder.setTypes(docType)
+		}
+		val matchAll = QueryBuilders.matchAllQuery()
+		queryBuilder.setQuery(matchAll)
+		queryBuilder.toString
+	}
+}
